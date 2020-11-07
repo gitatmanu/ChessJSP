55162b1dc79e226dcbc37e38f07b0ff36273a28c
Manel <manumcraft@gmail.com>
2020-11-07 13:37:48 +0100

    refactoring iterar torre alfil y reina hecho

--- src/main/java/Chess/Pieza.java
+++ src/main/java/Chess/Pieza.java
@@ -1,5 +1,8 @@
 package Chess;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Pieza {
 
         private Color color;
@@ -15,4 +18,41 @@ public abstract class Pieza {
         public Color getColor() {return this.color;}
         
         public abstract boolean[][] movimientosValidos(int y, int x, Partida partida);
+        
+        public static void iterar(int y, int x, int yDiferente, int xDiferente, List<int[]> movimientosAbsolutos, Partida partida) { 
+            
+            while(true) {
+                //si sale del array
+                if(yDiferente < 0 || yDiferente > 7 || xDiferente < 0 || xDiferente > 7) {
+                    break;
+                }   
+                //si pieza mismo color
+                if(partida.getTablero()[yDiferente][xDiferente] != null && partida.getTablero()[yDiferente][xDiferente].getColor() == partida.getTablero()[y][x].getColor()) {
+                   break; 
+                }
+                //si pieza color diferente
+                if(partida.getTablero()[yDiferente][xDiferente]
+                        != null
+                        && partida.getTablero()[yDiferente][xDiferente].getColor() != partida.getTablero()[y][x].getColor()) {
+                    movimientosAbsolutos.add(new int[]{yDiferente, xDiferente});
+                    break;           
+                }
+                //si es null
+                if(partida.getTablero()[yDiferente][xDiferente] == null) {
+                    movimientosAbsolutos.add(new int[]{yDiferente, xDiferente});
+                }
+               
+                if(yDiferente < y) {
+                    yDiferente--;
+                } else if (yDiferente > y) {
+                    yDiferente++;
+                }
+                
+                if(xDiferente < x) {
+                    xDiferente--;
+                } else if (xDiferente > x) {
+                    xDiferente++;
+                }    
+            }
+        }
 }
